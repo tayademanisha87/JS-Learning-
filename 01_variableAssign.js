9e112ac5cb2db4e806b5fcdcb33103ca63cecae2
tayademani <mwankhedkar87@gmail.com>
2023-07-19 17:03:17 +0530

    Assign_1
    Done

---
+++ 01_variableAssign.js
@@ -0,0 +1,40 @@
+
+
+var myFullName;
+console.log(myFullName);
+
+
+myFullName = "Manisha Tayade"
+console.log(myFullName);
+
+
+myFullName = "Manisha Santosh Tayade"
+console.log(myFullName);
+
+
+var myLove = "I Love Only JS"
+console.log(myLove);
+
+var firstName ="First Name: Manisha"
+console.log(firstName);
+
+var lastName ="Last Name: Tayade"
+console.log(lastName);
+
+var age ="Age: 37"
+console.log(age);
+
+var address ="Address: New Sangvi"
+console.log(address);
+
+var pincode ="Pincode: 411027"
+console.log(pincode);
+
+var gender ="Gender: Female"
+console.log(gender);
+
+var isMarried ="Married: Yes"
+console.log(isMarried);
+
+
+
