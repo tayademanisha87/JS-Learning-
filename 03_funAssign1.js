25ee7a49258c5bee80581378d1f757bcece98461
tayademani <mwankhedkar87@gmail.com>
2023-07-21 17:37:07 +0530

    done
    done

--- 03_funAssign1.js
+++ 03_funAssign1.js
@@ -1,4 +1,4 @@
-
+console.log('...........................Step:1........................................');
 function show(){
 
 console.log('Function with no argument and no return value');
@@ -7,4 +7,12 @@ console.log('City: Pune');
 
 }
 show();
-console.log('..........................................................................');
+console.log('..........................step:2.........................................');
+
+function personalDetails(){
+    console.log('First Name: Manisha' , 'Last Name: Tayade' , 'College Name: Jaihind C');
+
+}
+
+console.log('First Name: Manisha' , 'Last Name: Tayade' , 'College Name: Jaihind C');
+console.log('..........................step:3.........................................');
